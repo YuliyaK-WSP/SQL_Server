ec57e6630001533d5d50b9fc6d4bcc3594dd688e
Julia Koroleva <koroleva21.jv@gmail.com>
2023-03-19 21:38:41 +0300

    Создана процедура

---
+++ dbo.usp_MakeFamilyPurchase.sql
@@ -0,0 +1,23 @@
+USE [Sql_Test]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE OR ALTER PROCEDURE dbo.usp_MakeFamilyPurchase
+  (@FamilySurName varchar(255))
+AS
+BEGIN
+	DECLARE @cnt INT;
+	SELECT @cnt = COUNT(1)
+	  FROM dbo.Family f
+	 WHERE f.SurName = @FamilySurName;
+	IF @cnt > 0 
+		UPDATE dbo.Family
+		  SET BudgetValue = (SELECT SUM(b.Value)
+		                       FROM dbo.Basket b)
+		 WHERE SurName = @FamilySurName;
+	ELSE
+		RAISERROR ('Такой семьи нет', -1, -1);
+END
+GO
